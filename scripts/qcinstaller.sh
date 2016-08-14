ea0da05e4f5e2292abb0e2ba813644234b1056e6
Casper LI <casper.hk@outlook.com>
2016-08-14 13:43:51 +0800

    Create qcinstaller.sh

---
+++ scripts/qcinstaller.sh
@@ -0,0 +1,5 @@
+# Install Chinese Language
+sudo apt-get install language-pack-[cod] language-pack-gnome-[cod] language-pack-[cod]-base language-pack-gnome-[cod]-base 
+
+# Install quick method for fcitx
+sudo apt-get install fcitx-table-quick-classic
