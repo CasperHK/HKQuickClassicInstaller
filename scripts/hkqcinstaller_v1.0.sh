517fb2e4eb153fbd6fa5849fba54520fd6daee3a
Casper LI <casper.hk@outlook.com>
2016-08-14 13:52:22 +0800

    Update hkqcinstaller_v1.0.sh

--- scripts/hkqcinstaller_v1.0.sh
+++ scripts/hkqcinstaller_v1.0.sh
@@ -1,5 +1,10 @@
 # Install Chinese Language
 sudo apt-get install language-pack-[cod] language-pack-gnome-[cod] language-pack-[cod]-base language-pack-gnome-[cod]-base 
 
+# Switch input method system to fcitx
+
 # Install quick method for fcitx
 sudo apt-get install fcitx-table-quick-classic
+
+# Configuring fcitx
+
